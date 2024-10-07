a68a96e87b6c94922b3d39b0e84c0d93b5dea96c
root <root@LAPTOP-FV8JAK9N.>
2024-10-07 22:28:36 +0200

    temp get around

--- app.py
+++ app.py
@@ -5,21 +5,22 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the pipeline
-pipeline = joblib.load("california_housing_pipeline.pkl")
+pipeline = joblib.load('california_housing_pipeline.pkl')
+
+@app.route('/')
+def home():
+    return "Welcome to the California Housing Price Prediction API"
 
 
-# @app.route("/", methods=["GET", "POST"])
-# def home():
-#     return "Welcome to the California Housing Price Prediction API!"
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     data = request.get_json()
+#     if 'features' not in data:
+#         return jsonify({"error": "Missing 'features' key in request data"}), 400
 
+#     features = np.array(data['features']).reshape(1, -1)
+#     prediction = pipeline.predict(features)
+#     return jsonify({'prediction': prediction[0]})
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.get_json()
-    features = np.array(data["features"]).reshape(1, -1)
-    prediction = pipeline.predict(features)
-    return jsonify({"prediction": prediction[0]})
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
